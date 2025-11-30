2467ade395c451cb7d68e583b1aa59faef1f899f
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-30 14:47:50 -0500

    refactor: remove ingesta of websocket subscriptions

--- functions/export_orders.py
+++ functions/export_orders.py
@@ -6,7 +6,6 @@ dynamodb = boto3.resource("dynamodb")
 s3 = boto3.resource("s3")
 
 orders = dynamodb.Table(resource_name("orders"))
-ws_subscriptions = dynamodb.Table(resource_name("ws-order-subscriptions"))
 
 ingesta = s3.Bucket(resource_name("ingesta"))
 
@@ -15,9 +14,3 @@ def handler(event, context):
     orders_resp = orders.scan()
     body = "\n".join([to_json(order) for order in orders_resp["Items"]])
     ingesta.put_object(Key="orders/orders.json", Body=body)
-
-    subs_resp = ws_subscriptions.scan()
-    body = "\n".join([to_json(sub) for sub in subs_resp["Items"]])
-    ingesta.put_object(
-        Key="ws-order-subscriptions/ws-order-subscriptions.json", Body=body
-    )
